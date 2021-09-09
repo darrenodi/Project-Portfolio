31af5ebcf0fdb91e00b673e43cdfc4ea53ad3afb
darrenodi <tantheta85@gmail.com>
2021-09-09 10:40:46 +0100

    Add form email validation

--- form.js
+++ form.js
@@ -3,10 +3,12 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const emailValid = form.useremail.value;
+
   if (emailValid.toLowerCase() !== emailValid) {
     const errorType = form.querySelector('#email-error');
+
     errorType.textContent = 'Form not Sent!!!. Change email to lowercase.';
-    errorType.style.display = 'block';
+    errorType.style.display = 'inline';
   } else {
     form.submit();
   }
